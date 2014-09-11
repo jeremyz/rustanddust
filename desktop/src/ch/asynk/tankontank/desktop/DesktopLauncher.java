ccebde0500f6f2ce58619790f22a4e1a0a6a840f
Jérémy Zurcher <jeremy@asynk.ch>
2014-09-11 17:05:42 +0200

    desktop : adapt windows size to map size

--- desktop/src/ch/asynk/tankontank/desktop/DesktopLauncher.java
+++ desktop/src/ch/asynk/tankontank/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ch.asynk.tankontank.TankOnTank;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TankOnTank(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Tank On Tank";
+        config.width = 800;
+        config.height = 626;
+        new LwjglApplication(new TankOnTank(), config);
+    }
 }
