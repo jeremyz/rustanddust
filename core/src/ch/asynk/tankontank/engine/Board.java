946a0935ea29d8aa88a1bc0f40a5a104b697c718
Jérémy Zurcher <jeremy@asynk.ch>
2014-10-04 15:45:33 +0200

    Board: remove dead code

--- core/src/ch/asynk/tankontank/engine/Board.java
+++ core/src/ch/asynk/tankontank/engine/Board.java
@@ -397,15 +397,6 @@ public abstract class Board implements Disposable
         return getTile(coords).isOverlayEnabled(i);
     }
 
-    // public void disableOverlaysOn(GridPoint2 coords)
-    // {
-    //     Tile tile = getTile(coords);
-    //     if (tile.disableOverlays())
-    //         tilesToDraw.add(tile);
-    //     else
-    //         tilesToDraw.remove(tile);
-    // }
-
     public void enableOverlayOn(GridPoint2 coords, int i, boolean enable)
     {
         Tile tile = getTile(coords);
@@ -414,14 +405,6 @@ public abstract class Board implements Disposable
         else
             tilesToDraw.remove(tile);
     }
-
-    // public Pawn removeTopPawnFrom(GridPoint2 coords)
-    // {
-    //     Pawn pawn = getTopPawnAt(coords);
-    //     if (pawn != null)
-    //         removePawnFrom(pawn, coords);
-    //     return pawn;
-    // }
 
     public Pawn getTopPawnAt(GridPoint2 coords)
     {
@@ -444,11 +427,6 @@ public abstract class Board implements Disposable
         return n;
     }
 
-    // public Vector2 getTileCenter(GridPoint2 coords)
-    // {
-    //     return getTile(coords).getCenter();
-    // }
-
     public Vector2 getPawnPosAt(Pawn pawn, GridPoint2 coords, Vector2 pos)
     {
         Vector2 center = getTile(coords).getCenter();
@@ -461,20 +439,6 @@ public abstract class Board implements Disposable
         pawn.pushMove(pos.x, pos.y, o);
         pushPawnAt(pawn, coords);
     }
-
-    // public void movePawnTo(Pawn pawn, GridPoint2 coords)
-    // {
-    //     movePawnTo(pawn, coords, Orientation.KEEP);
-    // }
-
-    // public void movePawnTo(Pawn pawn, GridPoint2 coords, Orientation o)
-    // {
-    //     removePawnFrom(pawn, getHexAt(pawn.getCenter()));
-
-    //     pushPawnAt(pawn, coords);
-    //     Vector2 pos = getPawnPosAt(pawn, coords, null);
-    //     pawn.pushMove(pos.x, pos.y, o);
-    // }
 
     public void movePawn(final Pawn pawn, Vector<Vector3> path)
     {
@@ -489,21 +453,6 @@ public abstract class Board implements Disposable
         }));
         addPawnAnimation(pawn, seq);
     }
-
-    // public void resetPawnMoves(final Pawn pawn)
-    // {
-    //     removePawnFrom(pawn, getHexAt(pawn.getCenter()));
-
-    //     AnimationSequence seq = pawn.getResetMovesAnimation();
-    //     seq.addAnimation(RunnableAnimation.get(pawn, new Runnable() {
-    //         @Override
-    //         public void run() {
-    //             GridPoint2 coords = getHexAt(pawn.getCenter());
-    //             pushPawnAt(pawn, coords);
-    //         }
-    //     }));
-    //     addPawnAnimation(pawn, seq);
-    // }
 
     private GridPoint2 getHexAt(Vector2 v)
     {
