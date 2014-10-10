c7cdb725e1710c64ff7c18d0cd8f964d3234809a
Jérémy Zurcher <jeremy@asynk.ch>
2014-10-10 15:32:58 +0200

    GameCtrl.Config: add  showEnemyPossibilities

--- core/src/ch/asynk/tankontank/game/GameCtrl.java
+++ core/src/ch/asynk/tankontank/game/GameCtrl.java
@@ -19,14 +19,16 @@ public class GameCtrl implements Disposable
         public boolean showMoveAssists;
         public boolean canCancel;
         public boolean mustValidate;
+        public boolean showEnemyPossibilities;
 
         public Config()
         {
             this.showMoves = true;
             this.showTargets = true;
             this.showMoveAssists = true;
-            this.mustValidate = false;
             this.canCancel = true;
+            this.mustValidate = false;
+            this.showEnemyPossibilities = false;        // TODO
         }
     }
 
