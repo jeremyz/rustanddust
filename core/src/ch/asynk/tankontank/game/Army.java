f8d7d57a1fdd661b4e771e80d045c12ea57f9725
Jérémy Zurcher <jeremy@asynk.ch>
2014-10-17 09:22:33 +0200

    Army: add String and toString()

--- core/src/ch/asynk/tankontank/game/Army.java
+++ core/src/ch/asynk/tankontank/game/Army.java
@@ -2,10 +2,17 @@ package ch.asynk.tankontank.game;
 
 public enum Army
 {
-    GE, US;
+    GE("German"), US("US");
+    private String s;
+    Army(String s) { this.s = s; }
 
     public boolean isEnemy(Army other)
     {
         return (this != other);
+    }
+
+    public String toString()
+    {
+        return s;
     }
 }
