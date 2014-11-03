74c346057fd146f01b76688f6d97827801fcd5d8
Jérémy Zurcher <jeremy@asynk.ch>
2014-11-03 11:51:03 +0100

    BattleCommon: add entryPoints=ArrayList<TileSet>, and pawnEntry=HashMap<Unit, TileSet>

--- core/src/ch/asynk/tankontank/game/battles/BattleCommon.java
+++ core/src/ch/asynk/tankontank/game/battles/BattleCommon.java
@@ -1,9 +1,15 @@
 package ch.asynk.tankontank.game.battles;
 
 import java.util.Random;
+import java.util.HashMap;
+import java.util.ArrayList;
 
+import ch.asynk.tankontank.engine.TileSet;
 import ch.asynk.tankontank.game.Army;
 import ch.asynk.tankontank.game.Battle;
+import ch.asynk.tankontank.game.Player;
+import ch.asynk.tankontank.game.Unit;
+import ch.asynk.tankontank.game.Unit.UnitId;
 
 public abstract class BattleCommon implements Battle
 {
@@ -14,6 +20,8 @@ public abstract class BattleCommon implements Battle
     protected Army firstArmy;
     protected Army secondArmy;
     protected Factory factory;
+    protected ArrayList<TileSet> entryPoints = new ArrayList<TileSet>();
+    protected HashMap<Unit, TileSet> pawnEntry = new HashMap<Unit, TileSet>();
 
     public BattleCommon(Factory factory)
     {
@@ -39,5 +47,17 @@ public abstract class BattleCommon implements Battle
             firstArmy = Army.GE;
             secondArmy = Army.US;
         }
+    }
+
+    public void addEntryPoint(TileSet tiles)
+    {
+        entryPoints.add(tiles);
+    }
+
+    public void addReinforcement(Player player, TileSet entryPoint, UnitId unitId)
+    {
+        Unit unit = factory.getUnit(unitId);
+        player.addReinforcement(unit);
+        pawnEntry.put(unit, entryPoint);
     }
 }
