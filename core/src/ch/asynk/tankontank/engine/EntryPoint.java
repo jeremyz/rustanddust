91bbed26a1c275d13b54e23b3dd56b3530a33419
Jérémy Zurcher <jeremy@asynk.ch>
2014-11-12 22:26:39 +0100

    add engine/EntryPoint

---
+++ core/src/ch/asynk/tankontank/engine/EntryPoint.java
@@ -0,0 +1,11 @@
+package ch.asynk.tankontank.engine;
+
+public class EntryPoint extends TileSet
+{
+    public int allowedMoves;
+
+    public EntryPoint(Board board, int n)
+    {
+        super(board, n);
+    }
+}
