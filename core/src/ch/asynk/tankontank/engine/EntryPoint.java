bb0edecf047baa7b22745ab7608cd5424b5db76d
Jérémy Zurcher <jeremy@asynk.ch>
2014-11-16 00:01:57 +0100

    EntryPoint: add public Orientation orientation

--- core/src/ch/asynk/tankontank/engine/EntryPoint.java
+++ core/src/ch/asynk/tankontank/engine/EntryPoint.java
@@ -3,6 +3,7 @@ package ch.asynk.tankontank.engine;
 public class EntryPoint extends TileSet
 {
     public int allowedMoves;
+    public Orientation orientation;
 
     public EntryPoint(Board board, int n)
     {
