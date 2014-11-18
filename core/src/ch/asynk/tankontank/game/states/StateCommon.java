945f3d4a9120b2b83fca477f1d00edcf881a4ad6
Jérémy Zurcher <jeremy@asynk.ch>
2014-11-18 20:03:27 +0100

    StateCommon: add checkExit(Unit, Hex)

--- core/src/ch/asynk/tankontank/game/states/StateCommon.java
+++ core/src/ch/asynk/tankontank/game/states/StateCommon.java
@@ -1,5 +1,6 @@
 package ch.asynk.tankontank.game.states;
 
+import ch.asynk.tankontank.engine.Zone;
 import ch.asynk.tankontank.game.Map;
 import ch.asynk.tankontank.game.Hex;
 import ch.asynk.tankontank.game.Unit;
@@ -75,5 +76,20 @@ public abstract class StateCommon implements State
         map.hidePossibleMoves();
         map.hidePossibleTargets();
         map.hideMoveablePawns();
+    }
+
+    protected boolean checkExit(Unit unit, Hex hex)
+    {
+        Zone exitZone = ctrl.battle.getExitZone(unit);
+        if ((exitZone == null) || !exitZone.contains(hex))
+            return false;
+        if (map.possiblePaths.size() == 1) {
+            // TODO pathCost called with applayToPawn from Board it updates Pawn.move
+            int left = (unit.getMovementPoints() - map.possiblePaths.pathCost(0));
+            if (left < 1)
+                return false;
+        }
+        ctrl.setState(StateType.ESCAPE);
+        return true;
     }
 }
