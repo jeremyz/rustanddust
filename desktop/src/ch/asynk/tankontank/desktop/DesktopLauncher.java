cc31895064ce8f88577ce64a2a9af832879c01ed
Jérémy Zurcher <jeremy@asynk.ch>
2014-12-10 22:46:33 +0100

    desktop: 1024x768

--- desktop/src/ch/asynk/tankontank/desktop/DesktopLauncher.java
+++ desktop/src/ch/asynk/tankontank/desktop/DesktopLauncher.java
@@ -9,8 +9,10 @@ public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Tank On Tank";
-        config.width = 800;
-        config.height = 682;
+        // config.width = 800;
+        // config.height = 600;
+        config.width = 1024;
+        config.height = 768;
         config.addIcon("data/icon.png", Files.FileType.Internal);
         new LwjglApplication(new TankOnTank(), config);
     }
