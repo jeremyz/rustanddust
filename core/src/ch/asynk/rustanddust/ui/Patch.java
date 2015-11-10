cd0cd844fbe1b4812ba211567276ca3074d32f17
Jérémy Zurcher <jeremy@asynk.ch>
2015-11-10 11:32:41 +0100

    Patch: add setBtnLeft(Bg) and setBtnRight(Bg)

--- core/src/ch/asynk/rustanddust/ui/Patch.java
+++ core/src/ch/asynk/rustanddust/ui/Patch.java
@@ -25,4 +25,14 @@ public class Patch extends Widget
         if (!visible) return;
         patch.draw(batch, rect.x, rect.y, rect.width, rect.height);
     }
+
+    public void setBtnRight(Bg btn)
+    {
+        btn.setPosition((rect.x + rect.width - (btn.getWidth() * 0.666f)), (rect.y -  (btn.getHeight() * 0.333f)));
+    }
+
+    public void setBtnLeft(Bg btn)
+    {
+        btn.setPosition((rect.x - (btn.getWidth() * 0.333f)), (rect.y -  (btn.getHeight() * 0.333f)));
+    }
 }
