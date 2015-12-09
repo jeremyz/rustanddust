41aa097a355a37fe930d079ad21c11464222b855
Jérémy Zurcher <jeremy@asynk.ch>
2015-12-09 11:40:02 +0100

    StateSelect: no need of HQ activation extra click

--- core/src/ch/asynk/rustanddust/game/states/StateSelect.java
+++ core/src/ch/asynk/rustanddust/game/states/StateSelect.java
@@ -87,15 +87,9 @@ public class StateSelect extends StateCommon
 
         isEnemy = ctrl.battle.getPlayer().isEnemy(unit);
         if (!isEnemy && (unit == selectedUnit) && unit.canMove()) {
-            if (unit.isHq()) {
-                ctrl.hud.notify("HQ activation");
-                select(upHex, unit, isEnemy);
-                ctrl.setState(StateType.MOVE);
-            } else {
-                // quick rotate
-                to = upHex;
-                ctrl.setState(StateType.ROTATE);
-            }
+            // quick rotate
+            to = upHex;
+            ctrl.setState(StateType.ROTATE);
         } else {
             select(upHex, unit, isEnemy);
             ctrl.hud.notify(selectedUnit.toString());
