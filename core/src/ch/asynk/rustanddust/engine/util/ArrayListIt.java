1619921f6acda0b328f10b34339c2ffd7f06d238
Jérémy Zurcher <jeremy@asynk.ch>
2016-01-09 11:45:16 +0100

    add engine/util/ArrayListIt

---
+++ core/src/ch/asynk/rustanddust/engine/util/ArrayListIt.java
@@ -0,0 +1,49 @@
+package ch.asynk.rustanddust.engine.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListIt<E> extends ArrayList<E> implements Iterator, Iterable<E>
+{
+    private int i;
+    private int s;
+
+    public ArrayListIt()
+    {
+        super();
+    }
+
+    public ArrayListIt(int n)
+    {
+        super(n);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Iterator<E> iterator()
+    {
+        this.i = 0;
+        this.s = size();
+        return (Iterator<E>) this;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+        return (i < s);
+    }
+
+    @Override
+    public E next()
+    {
+        E e = get(i);
+        i += 1;
+        return e;
+    }
+
+    @Override
+    public void remove()
+    {
+        throw new RuntimeException("Iterator.remove is not allowed in here bitch!");
+    }
+}
