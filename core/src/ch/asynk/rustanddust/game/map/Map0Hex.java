c49aaeaabd83e085e23ef0eca169ce804cf78ab4
Jérémy Zurcher <jeremy@asynk.ch>
2016-02-02 14:56:28 +0100

    Map0Hex: remove IterableArray<Hex> objectives()

--- core/src/ch/asynk/rustanddust/game/map/Map0Hex.java
+++ core/src/ch/asynk/rustanddust/game/map/Map0Hex.java
@@ -69,11 +69,6 @@ public abstract class Map0Hex extends Board
         return n;
     }
 
-    public IterableArray<Hex> objectives()
-    {
-        return objectives;
-    }
-
     public void hexSelect(Hex hex)          { selectedTile.set(hex); }
     public void hexUnselect(Hex hex)        { selectedTile.hide(); }
     public void hexMoveShow(Hex hex)        { enableOverlayOn(hex, Hex.MOVE, true); }
