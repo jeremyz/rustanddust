76b036db2e9f632fb8315f7ff925434912671940
Jérémy Zurcher <jeremy@asynk.ch>
2016-02-22 15:33:59 +0100

    AndroidBackend: fetch owner's google account

--- android/src/ch/asynk/rustanddust/android/AndroidBackend.java
+++ android/src/ch/asynk/rustanddust/android/AndroidBackend.java
@@ -1,6 +1,10 @@
 package ch.asynk.rustanddust.android;
 
 import android.app.Application;
+import android.content.Context;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
 
 import ch.asynk.rustanddust.RustAndDust;
 import ch.asynk.rustanddust.util.Backend;
@@ -19,9 +23,17 @@ public class AndroidBackend implements Backend
     public int getMyId() { return me; }
     public int getOpponentId() { return opponent; }
 
+    @Override
     public void init(RustAndDust game)
     {
-        me = game.db.storePlayerGetId("me", "myself");
+        AccountManager aMgr = (AccountManager) app.getSystemService(Context.ACCOUNT_SERVICE);
+        Account[] accounts = aMgr.getAccountsByType("com.google");
+        for (Account account : accounts)
+            RustAndDust.debug("account : " + account.toString());
+        if (accounts.length > 0)
+            me = game.db.storePlayerGetId(accounts[0].name.toString(), "myself");
+        else
+            me = game.db.storePlayerGetId("me", "myself");
         opponent = game.db.storePlayerGetId("opponent", "other");
     }
 }
